8ad2ed1be6b7b45289546e01dc878274b491fb15
Wing <17802598211@163.com>
2017-04-28 23:31:30 +0800

    本次提交改了java的代码关于乙级第五题 并且加了c++代码的实现

--- 3np1c/Guess.cpp
+++ 3np1c/Guess.cpp
@@ -1,38 +1,19 @@
-package cn.bemegood.guess.mian;
-
-import java.util.Scanner;
-
-
-
-public class Guess {
-	
-	
-
-	public static void main(String[] args){
-
-		Process pro = new Process();
-//		int[] n =new int[100];
-//		for(int i=0;i<99;i++){
-//			n[i] = 3+i*i;
-//		}
-//		Process pro = new Process(99, n);
-		pro.process();
-		pro.sort();
-		pro.output();
-		
-	}
-	
-
-}
+/*
+ * Guess.cpp
+ *
+ *  Created on: 2017年4月28日
+ *      Author: Administrator
+ */
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
 class Process{
-//	private int[] n = new int[100];
-//	private int num = 0;
-	private int[] n = new int[100];
-	private int num = 0;
-
+private:
+	int* n;
+	int num;
 	//将x从指定成员数组n中删除
-	private int removeSame(int x,int where){
+	int removeSame(int x,int where){
 		for(int i=0;i<num;i++){
 			if(x==n[i]){		//如果查找到x了 那么就将其删除 num--
 				for(int j=i;j<num-1;j++){
@@ -41,33 +22,31 @@ class Process{
 				num--;
 				if(i<where){	//如果删除的数在已搜索位子的前面，那么返回1标记
 					return 1;
-				}else{
-					return 0;
 				}
 			}
 		}
 		return 0;
 	}
-	
+
 	//奇偶判断函数，奇数就返回1，偶数返回0  输入那个数字
-	private boolean odevity(int n){
+	bool odevity(int n){
 		if(n%2==0){
 			return false;
 		}else{
 			return true;
 		}
 	}
-	
+
+public:
 	//输入
-	public void input(){
-		Scanner scan = new Scanner(System.in);
-		num = scan.nextInt();
+	void input(){
+		cin >> num;
+		n = new int[num];
 		for(int i=0;i<num;i++){
-			n[i] = scan.nextInt(); 
+			cin >> n[i];
 		}
-		scan.close();
 	}
-	public void sort(){
+	void sort(){
 		//冒泡排序 我去竟然差点连冒泡都不会了，这个泡在for 0:n的过程中只能冒向后面
 		for(int i=0;i<num-1;i++){
 			for(int j=0;j<num-1-i;j++){
@@ -79,31 +58,30 @@ class Process{
 			}
 		}
 	}
-	
 	//输出
-	public void output(){
+	void output(){
 		for(int i=0;i<num-1;i++){
-			System.out.print(n[i]+" ");
+//			System.out.print(n[i]+" ");
+			cout << n[i] << " ";
 		}
-		System.out.print(n[num-1]);		//这个独立仅为了满足输出要求
+//		System.out.print(n[num-1]);		//这个独立仅为了满足输出要求
+		cout << n[num-1] << endl;
 	}
 	//构造
-	public Process(){
+	Process(){
 		input();
 	}
-	
-	public Process(int num,int[] n){
-		this.num = num;
-		this.n = n;			//这是引用赋值 注意
-	}
-	
 
+//	Process(int num,int* n){
+//		this.num = num;
+//		this.n = n;			//这是引用赋值 注意
+//	}
 
 	//具体执行函数
-	public void process(){
-		int i=0; 
+	void process(){
+		int i=0;
 		int temp;
-		
+
 		for(i=0;i<num;i++){		//num在removeSame中改变
 			temp = n[i];
 			while(temp != 1){
@@ -117,7 +95,26 @@ class Process{
 			}
 		}
 	}
-}
+};
+
+
+	int main( ){
+
+//		Process pro = new Process();
+		Process* pro = new Process();
+//		int[] n =new int[100];
+//		for(int i=0;i<99;i++){
+//			n[i] = i+2;
+//		}
+//		Process pro = new Process(99, n);
+		pro->process();
+		pro->sort();
+		pro->output();
+		return 0;
+
+	}
+
+
 
 
 
